4ab09a0a97a9701f75e05c7fd0521cd9bbfde943
Ori Hoch <ori@uumpa.com>
2019-05-19 20:47:44 +0300

    Run the tests using cloudcli v0.1.1

--- .travis.sh
+++ .travis.sh
@@ -2,15 +2,17 @@
 
 DOCKER_IMAGE=cloudwm/cloudcli-server
 
+CLOUDCLI_VERSION=v0.1.1
+
 if [ "${1}" == "script" ]; then
     docker pull ${DOCKER_IMAGE}:latest
     ! docker build --cache-from ${DOCKER_IMAGE}:latest -t ${DOCKER_IMAGE}:latest -t ${DOCKER_IMAGE}:${TRAVIS_COMMIT} . && exit 1
     echo "Running cloudcli tests"
     docker run -d --sig-proxy=false -e CLOUDCLI_PROVIDER=proxy -e CLOUDCLI_API_SERVER=$TEST_API_SERVER -p 8080:80 ${DOCKER_IMAGE}:${TRAVIS_COMMIT}
     sleep 5
-    git clone https://github.com/cloudwm/cloudcli.git -b v0.0.4
+    git clone https://github.com/cloudwm/cloudcli.git -b $CLOUDCLI_VERSION
     cd cloudcli
-    wget https://github.com/cloudwm/cloudcli/releases/download/v0.0.4/cloudcli-linux-amd64 -O cloudcli
+    wget https://github.com/cloudwm/cloudcli/releases/download/$CLOUDCLI_VERSION/cloudcli-linux-amd64 -O cloudcli
     sudo chown $USER ./cloudcli && sudo chmod +x ./cloudcli
     export PATH="`pwd`:${PATH}"
     export DEBUG_OUTPUT_FILE=/dev/null
